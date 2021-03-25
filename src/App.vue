5117f75d3d07b464c051a67f993c8c4fae2c33bd
zhanglx <zhanglx@guohe.org>
2021-03-25 17:31:18 +0800

    阶段提交2021-03-25

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-      <column-list :list="list"></column-list>
+    <global-header :user="currentUser"></global-header>
+    <column-list :list="list"></column-list>
   </div>
 </template>
 
@@ -8,32 +9,52 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 const testData: ColumnProps[] = [
-    {
-        id: 1,
-        title: 'test1的专栏',
-        description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-    },
-    {
-        id: 2,
-        title: 'test2的专栏',
-        description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-    }
-]
-export default defineComponent({
-  name: 'App',
-  components: {
-    ColumnList
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   },
-  setup() {
-      return {
-          list: testData
-      }
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 3,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 4,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
-  
-})
+]
+
+const user: UserProps = {
+  isLogin: false
+}
+export default defineComponent(
+  {
+    name: 'App',
+    components: {
+      ColumnList,
+      GlobalHeader
+    },
+    setup () {
+      return {
+        list: testData,
+        currentUser: user
+      }
+    }
+  }
+)
 </script>
 
 <style>
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,32 +1,53 @@
 <template>
-    <div class="row">
-        <div v-for="column in list" :key="column.id" class="col-4">
-          <img :src="column.avatar" :alt="column.title">
-          <h5>{{ column.title }}</h5>
-          <p>{{ column.description }}</p>
-          <a href="#">进入专栏</a>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3">
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-start">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
+// 创建一个列道具接口，表明每列道具框中包含哪些参数
 export interface ColumnProps {
     id: number;
     title: string;
-    avatar: string;
+    avatar?: string;
     description: string;
 }
-
-export default defineComponent({
+export default defineComponent(
+  {
     name: 'ColumnList',
     props: {
-        list: {
-            type: Array as PropType<ColumnProps[]>,
-            required: true
-        }
+      list: {
+      // 使用类型断言给数组确定类型
+      // Array是一个数组构造函数不是类型，使用 PropType 方法，它接受一个泛型，将 Array 构造函数返回传入的泛型类型
+        type: Array as PropType<ColumnProps[]>,
+        required: true
+      }
+    },
+    setup (props) {
+      const columnList = computed(() => {
+        return props.list.map((column) => {
+          if (!column.avatar) {
+            column.avatar = require('@/assets/avatar.jpg')
+          }
+          return column
+        })
+      })
+      return {
+        columnList
+      }
     }
-})
+  }
+)
 </script>
 
 <style>
